d0e80381c299678e1fc1478d8527c73e0e99f92e
Thales Macêdo <thalesmacedo1@live.com>
2022-01-22 16:38:15 -0300

    Initial commit

---
+++ src/test/kotlin/com/example/restfulapispringkotlin/RestfulApiSpringKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.restfulapispringkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestfulApiSpringKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
