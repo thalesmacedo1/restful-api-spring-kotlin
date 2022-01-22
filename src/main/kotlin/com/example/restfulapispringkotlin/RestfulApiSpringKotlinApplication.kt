7712b0808dc6050b4d051d49a8e6466f7540cfd8
Thales Macêdo <thalesmacedo1@live.com>
2022-01-22 18:48:25 -0300

    Create a data class and a controller﻿

--- src/main/kotlin/com/example/restfulapispringkotlin/RestfulApiSpringKotlinApplication.kt
+++ src/main/kotlin/com/example/restfulapispringkotlin/RestfulApiSpringKotlinApplication.kt
@@ -2,10 +2,25 @@ package com.example.restfulapispringkotlin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-class RestfulApiSpringKotlinApplication
+class DemoApplication
 
 fun main(args: Array<String>) {
-	runApplication<RestfulApiSpringKotlinApplication>(*args)
+	runApplication<DemoApplication>(*args)
 }
+
+@RestController
+class MessageResource {
+	@GetMapping
+	fun index(): List<Message> = listOf(
+		Message("1", "Hello!"),
+		Message("2", "Bonjour!"),
+		Message("3", "Privet!"),
+	)
+}
+
+data class Message(val id: String?, val text: String)
